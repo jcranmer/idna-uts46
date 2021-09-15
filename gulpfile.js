aa9b6cddcab38b78240fd7ce5f0d9a21728bf4c9
Rashad Jamara <rjamara@hexonet.net>
2021-09-15 13:03:21 +0200

    build(gulp): introduced

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const {dest, src, watch, series} = require('gulp');
+const gulpEsbuild = require('gulp-esbuild');
+const prettier = require('gulp-prettier');
+const prettiercfg = require('./.prettierrc');
+const filesForPrettier = ['test/*.js', '*.js', '.*.js', '!uts46bundle.min.js'];
+
+/**
+ * format code using prettier
+ */
+exports.prettierCheck = function () {
+	return (
+		src(filesForPrettier)
+			//.pipe(debug())
+			.pipe(prettier.check(prettiercfg))
+			.pipe(dest(file => file.base))
+	);
+};
+
+/**
+ * format code using prettier
+ */
+exports.prettier = function () {
+	return src(filesForPrettier)
+		.pipe(prettier(prettiercfg))
+		.pipe(dest(file => file.base));
+};
+
+/**
+ * Esbuild minify + bundle css and js files
+ */
+exports.bundle = function () {
+	return src('./uts46.js')
+		.pipe(
+			gulpEsbuild({
+				outfile: 'uts46bundle.min.js',
+				bundle: true,
+				minify: true,
+				format: 'iife',
+				platform: 'browser',
+				globalName: 'ispapiIdnconverter',
+				sourcemap: true,
+				target: 'es2015',
+				resolveExtensions: ['.js'],
+				allowOverwrite: true,
+			}),
+		)
+		.pipe(dest('.'));
+};
+
+/**
+ * watch for any changes, minify + bundle + concatinate
+ */
+
+exports.watcher = function () {
+	watch('./uts46.js', series(exports.prettier, exports.bundle));
+};
