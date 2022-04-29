4586ca32ae3a2cf971da8af1c12983d064999fbb
Kai Schwarz <kschwarz@hexonet.net>
2022-04-29 13:00:31 +0200

    ci(npm / gh actions): consider package-lock.json in CI/CD

--- release.config.js
+++ release.config.js
@@ -7,7 +7,11 @@ cfg.plugins.unshift([
 ]);
 cfg.plugins = cfg.plugins.map(plugin => {
 	if (plugin[0] === '@semantic-release/git') {
-		plugin[1].assets.push('uts46bundle.js', 'uts46bundle.min.js');
+		plugin[1].assets.push(
+			'uts46bundle.js',
+			'uts46bundle.min.js',
+			'package-lock.json', // can be removed when covered in semantic-release-github-npm-config
+		);
 	}
 	return plugin;
 });
