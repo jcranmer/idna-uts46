bd3f43a8de68d5defe8245a7acf6d539e63799d7
Kai Schwarz <kschwarz@hexonet.net>
2023-03-09 14:56:18 +0100

    fix(build): added missing semantic-release configuration file and typings file

---
+++ release.config.js
@@ -0,0 +1,14 @@
+const cfg = require('@hexonet/semantic-release-github-npm-config');
+cfg.plugins.unshift([
+  '@semantic-release/exec',
+  {
+    prepareCmd: 'npm run build && npx lint-staged',
+  },
+]);
+cfg.plugins = cfg.plugins.map((plugin) => {
+  if (plugin[0] === '@semantic-release/git') {
+    plugin[1].assets.push('dist/index.d.ts', 'dist/index.bundle.js');
+  }
+  return plugin;
+});
+module.exports = cfg;
